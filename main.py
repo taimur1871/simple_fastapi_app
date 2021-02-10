741743383abcf081f63f98f82fac2fbe294f8911
Rana Ahmad <taimur1871@gmail.com>
2021-02-10 11:21:10 -0700

    update main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26
+"""
+
+# 1. Library imports
+#from starlette.responses import FileResponse
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+# 2. Create app and model objects
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates/")
+
+#3. Welcome page
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request:Request):
+    return templates.TemplateResponse("welcome.html", {"request": request, "message":"message"})
+
+@app.post("/uploads")
+async def create_upload_file(request:Request):
+    
+    return templates.TemplateResponse("upload_page.html", {"request": request})
